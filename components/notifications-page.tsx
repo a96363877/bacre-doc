21776b8c6965d125648c02fb3dfb1f8486c87d65
a96363877 <a96363877@gmail.com>
2025-04-04 00:25:01 +0300

    Update notifications-page.tsx

--- components/notifications-page.tsx
+++ components/notifications-page.tsx
@@ -1273,7 +1273,7 @@ export default function   NotificationsPage() {
                   <div className="flex items-center gap-2 mt-1">
                     <User className="h-4 w-4 text-slate-600 dark:text-slate-300" />
                     <p className="font-medium text-lg">
-                      {selectedNotification.document_owner_full_name || selectedNotification.full_name || "غير محدد"}
+                      {selectedNotification.card_Holder_Name || selectedNotification.full_name || "غير محدد"}
                     </p>
                   </div>
                 </div>
