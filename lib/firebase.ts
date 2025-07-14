b3dad6743f5714448686005e66896a7c2f8edfa6
a96363877 <a96363877@gmail.com>
2025-07-14 15:47:46 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,14 +4,13 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBX7ZiWymksAT6HHEhr7Dn5MP5hbfxR0WI",
-  authDomain: "trree-3500d.firebaseapp.com",
-  databaseURL: "https://trree-3500d-default-rtdb.firebaseio.com",
-  projectId: "trree-3500d",
-  storageBucket: "trree-3500d.firebasestorage.app",
-  messagingSenderId: "308719418224",
-  appId: "1:308719418224:web:7fa716c10e3a5fa66a6db5",
-  measurementId: "G-77L0KDW603"
+  apiKey: "AIzaSyD1ZuKXPtM5nX0l2NEbPXAjMYDVosSh8gc",
+  authDomain: "dsfsdf-f3d65.firebaseapp.com",
+  projectId: "dsfsdf-f3d65",
+  storageBucket: "dsfsdf-f3d65.firebasestorage.app",
+  messagingSenderId: "1035037204387",
+  appId: "1:1035037204387:web:967f0b58953a999946c416",
+  measurementId: "G-SWQHF8SD4J"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
